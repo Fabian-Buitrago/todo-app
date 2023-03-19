8172b793d2fe76deaa44212640fb95de40d5f2aa
fbuitrago <fabianbuitragov@gmail.com>
2023-03-18 23:59:53 -0500

    Create todo component witout styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { Input } from "./components/Input";
 import { List } from "./components/List";
 import { Title } from "./components/Title";
+import { Todo } from "./components/Todo/Todo";
 
 function App() {
   return (
-    <div className="bg-gray-900 min-h-screen h-full text-gray-100 flex items-center justify-center py-20 px-5">
+    <div className="bg-gray-900 min-h-screen h-full font-inter text-gray-100 flex items-center justify-center py-20 px-5">
       <div className="container flex flex-col max-w-xl">
         <Title />
         <Input />
         <List>
-          <h2>Todos List</h2>
+          <Todo />
         </List>
       </div>
     </div>
