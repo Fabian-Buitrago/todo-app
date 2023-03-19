18e19608b7f0562d154db84d8b3b3ed8a6467be4
fbuitrago <fabianbuitragov@gmail.com>
2023-03-18 23:40:55 -0500

    Create input component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { Input } from "./components/Input";
 import { Title } from "./components/Title";
 
 function App() {
   return (
-    <div className="bg-gray-900 min-h-screen h-full text-gray-100 flex items-center py-20 px-5">
+    <div className="bg-gray-900 min-h-screen h-full text-gray-100 flex items-center justify-center py-20 px-5">
       <div className="container flex flex-col max-w-xl">
         <Title />
+        <Input />
       </div>
     </div>
   );
